62406c5b4afaa8b0b0f84a8c35e32ddca1816444
mashuk-tamim <tamimmashuk328@gmail.com>
2023-09-11 19:35:20 +0600

    country name and flag are shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+
 import './App.css'
+import Countries from './assets/components/Countries/Countries'
 
 function App() {
 
   return (
     <>
       <h1>React World Tour</h1>
+      <Countries></Countries>
     </>
   )
 }
